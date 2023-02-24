78287b5f164570f6af2d371629f6536b23759154
ChunLienChiang <chunlienchiang0113@gmail.com>
2023-02-24 16:46:44 +0800

    feat: add RegionMask_PW in tool_RegionMask

--- tools/tool_RegionMask.py
+++ tools/tool_RegionMask.py
@@ -36,9 +36,48 @@ def Create_RegionMask_IO(Lat, Lon, LandFraction):
 	RegionMask = np.where((Lat[:, None]>=-30)&(Lat[:, None]<=30)&(Lon[None, :]>=30)&(Lon[None, :]<=120), True, False)
 
 	# Exclude the north-east corner of array
-	RegionMask = np.where((Lat[:, None]>=-9)&(Lon[None, :]>=105), False, RegionMask)
+	RegionMask = np.where((Lat[:, None]>=-8)&(Lon[None, :]>=105), False, RegionMask)
 	RegionMask = np.where((Lat[:, None]>=3)&(Lon[None, :]>=98), False, RegionMask)
 	RegionMask = np.where((Lat[:, None]>=-1)&(Lon[None, :]>=101), False, RegionMask)
+
+	# Land mask
+	LandMask   = tool_CP.Get_LandMask(LandFraction, MaskType='Ocean')
+	RegionMask = np.where(LandMask, RegionMask, False)
+	
+	return RegionMask
+
+def Create_RegionMask_WP(Lat, Lon, LandFraction):
+
+	"""
+	Create the regional mask for Western Pacific (WP) region
+	==========================
+	Argument:
+		
+		Lat (numpy array): latitude
+		
+		Lon (numpy array): longitude
+
+		LandFraction (numpy array): land fraction
+
+	Output:
+
+		RegionMask (numpy array): the regional mask
+	==========================
+	"""
+
+	if (LandFraction.shape != (Lat.size, Lon.size)):
+
+		raise ValueError('The shape of LandFraction must equal to (latitude, longitude)')
+	
+	# Fill in a rectangle region
+	RegionMask = np.where((Lat[:, None]>=-20)&(Lat[:, None]<=20)&(Lon[None, :]>=100)&(Lon[None, :]<=170), True, False)
+
+	# Exclude the north-east corner of array
+	RegionMask = np.where((Lat[:, None]<=-10)&(Lon[None, :]<=120), False, RegionMask)
+	RegionMask = np.where((Lat[:, None]<=-8)&(Lon[None, :]<=114), False, RegionMask)
+	RegionMask = np.where((Lat[:, None]<=-6)&(Lon[None, :]<=108), False, RegionMask)
+	RegionMask = np.where((Lat[:, None]<=-5)&(Lon[None, :]<=104), False, RegionMask)
+	RegionMask = np.where((Lat[:, None]<=-2)&(Lon[None, :]<=103), False, RegionMask)
 
 	# Land mask
 	LandMask   = tool_CP.Get_LandMask(LandFraction, MaskType='Ocean')
@@ -91,5 +130,7 @@ if (__name__ == '__main__'):
 	RefInfo = tool_CP.Get_RefInfo()
 
 	RegionMask_IO = Create_RegionMask_IO(RefInfo['Lat'], RefInfo['Lon'], RefInfo['LandFraction'])
-
 	Output_RegionMask('IO', RegionMask_IO)
+
+	RegionMask_WP = Create_RegionMask_WP(RefInfo['Lat'], RefInfo['Lon'], RefInfo['LandFraction'])
+	Output_RegionMask('WP', RegionMask_WP)
