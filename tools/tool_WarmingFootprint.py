ceb001b66b188514f34253cf003522ff49dd4fd8
ChunLienChiang <chunlienchiang0113@gmail.com>
2023-03-01 13:53:55 +0800

    feat: seasonal warming footprint

--- tools/tool_WarmingFootprint.py
+++ tools/tool_WarmingFootprint.py
@@ -10,17 +10,15 @@ import os
 import statsmodels.api as sm
 import tool_ClimData_Preprocessing as tool_CP
 
-def Create_WarmingFootprint(Data_TS, Data_Time=None, Data_TimeRange=None):
+def Create_WarmingFootprint(Data_TS):
 
 	"""
 	Create the warming footprint
 	==========================
 	Argument:
 		
-		Data_TS (numpy array): 3-d array of TS data
+		Data_TS (numpy array): 3-d array of TS data, monthly data
 		
-		Data_Time (numpy array): optional, 1-d array of time
-
 	Output:
 
 		WarmingFootprint (numpy array): 2-d array of the warming footprint
@@ -31,56 +29,32 @@ def Create_WarmingFootprint(Data_TS, Data_Time=None, Data_TimeRange=None):
 	if (np.ndim(Data_TS) != 3):
 
 		raise ValueError('The number of the argument Data_TS must be 3.')
-
-	# Check whether the argument Data_Time is given
-	if (not Data_Time is None):
-
-		if (np.ndim(Data_Time) != 1):
-
-			raise ValueError('The number of the argument Data_Time must be 1.')
-
-		elif (Data_Time.shape[0] != Data_TS.shape[0]):
-
-			raise ValueError('The length of the first dimension of the argument Data_TS must equal to the length of the argument Data_Time.')
 	
-	else:
+	if (Data_TS.shape[0] % 12 != 0):
 
-		Data_Time = np.arange(Data_TS.shape[0])
-	
-	# Check whether the argument Data_TimeRange is correct
-	if (not Data_TimeRange is None):
+		raise ValueError('The length of the first dimension of the argument Data_TS must be a multiple of 12.')
 
-		if (not isinstance(Data_TimeRange, list)):
-
-			raise ValueError('The argument Data_TimeRange must be a list.')
-
-		if not (isinstance(Data_TimeRange[0], str) and isinstance(Data_TimeRange[1], str)):
-
-			raise ValueError('The items in the argument Data_TimeRange must be strings.')
-
-		# Crop data along time dimension
-		Data_TS   = Data_TS[(Data_Time>=int(Data_TimeRange[0]))&(Data_Time<=int(Data_TimeRange[1])), ...]
-		Data_Time = Data_Time[(Data_Time>=int(Data_TimeRange[0]))&(Data_Time<=int(Data_TimeRange[1])), ...]
-	
 	# Create new arrays to save regression coefficients and p-values
-	Footprint        = np.full(Data_TS.shape[1:], np.nan)
-	Footprint_pvalue = np.full(Data_TS.shape[1:], np.nan)
+	Footprint        = np.full((12, *Data_TS.shape[1:]), np.nan)
+	Footprint_pvalue = np.full((12, *Data_TS.shape[1:]), np.nan)
 
 	# Get land mask
 	LandMask         = tool_CP.Get_LandMask(MaskType='Ocean')
 
 	# Calculate the linear regression of each grid point
-	for ind_Lat in np.arange(Data_TS.shape[-2]):
+	for ind_Month in np.arange(12):
 
-		for ind_Lon in np.arange(Data_TS.shape[-1]):
-			
-			# Check whether the grid point is in the ocean region
-			if (LandMask[ind_Lat, ind_Lon] is False): continue
+		for ind_Lat in np.arange(Data_TS.shape[-2]):
 
-			# OLS
-			Model_Results                      = sm.OLS(Data_TS[:, ind_Lat, ind_Lon], sm.add_constant(Data_Time)).fit()
-			Footprint[ind_Lat, ind_Lon]        = Model_Results.params[1]
-			Footprint_pvalue[ind_Lat, ind_Lon] = Model_Results.pvalues[1]
+			for ind_Lon in np.arange(Data_TS.shape[-1]):
+				
+				# Check whether the grid point is in the ocean region
+				if (LandMask[ind_Lat, ind_Lon] is False): continue
+
+				# OLS
+				Model_Results                                 = sm.OLS(Data_TS[ind_Month::12, ind_Lat, ind_Lon], sm.add_constant(np.arange(Data_TS.shape[0]//12))).fit()
+				Footprint[ind_Month, ind_Lat, ind_Lon]        = Model_Results.params[1]
+				Footprint_pvalue[ind_Month, ind_Lat, ind_Lon] = Model_Results.pvalues[1]
 
 	return Footprint, Footprint_pvalue
 
@@ -112,16 +86,17 @@ def Output_WarmingFootprint(Footprint, Footprint_pvalue, FilePath='../src/Warmin
 	ncFile.title = 'Warming footprint'
 	
 	# Create new dimensions
-	Dim_Lat = ncFile.createDimension('Lat', Footprint.shape[0])
-	Dim_Lon = ncFile.createDimension('Lon', Footprint.shape[1])
+	Dim_Month = ncFile.createDimension('Month', Footprint.shape[0])
+	Dim_Lat   = ncFile.createDimension('Lat', Footprint.shape[-2])
+	Dim_Lon   = ncFile.createDimension('Lon', Footprint.shape[-1])
 
 	# Create new variables
-	Var_Footprint = ncFile.createVariable('WarmingFootprint', np.float32, ('Lat', 'Lon'))
+	Var_Footprint = ncFile.createVariable('WarmingFootprint', np.float32, ('Month', 'Lat', 'Lon'))
 	Var_Footprint.long_name = 'warming footprint (the linear trend of TS)'
-	Var_Footprint.units = 'K per month'
+	Var_Footprint.units = 'K per year'
 	Var_Footprint[:] = Footprint
 
-	Var_Footprint_pvalue = ncFile.createVariable('WarmingFootprint_pvalue', np.float32, ('Lat', 'Lon'))
+	Var_Footprint_pvalue = ncFile.createVariable('WarmingFootprint_pvalue', np.float32, ('Month', 'Lat', 'Lon'))
 	Var_Footprint_pvalue.long_name = 'pvalues of warming footprint'
 	Var_Footprint_pvalue[:] = Footprint_pvalue
 
@@ -145,13 +120,15 @@ if (__name__ == '__main__'):
 		FileName='f.e21.FHIST_BGC.f09_f09.historical.ersstv5.goga.ens01.cam.h0.TS.188001-201412.nc', \
 		Var='TS', \
 	)
-	Data_Date = tool_CP.Get_RefData(\
+	Data_Time = tool_CP.Get_RefData(\
 		FileName='f.e21.FHIST_BGC.f09_f09.historical.ersstv5.goga.ens01.cam.h0.TS.188001-201412.nc', \
 		Var='date', \
 	)
-
+	
+	Data_TS   = Data_TS[(Data_Time>=int(Data_TimeRange[0]))&(Data_Time<=int(Data_TimeRange[1])), ...]
+	
 	# Calculate warming footprint
-	Footprint, Footprint_pvalue = Create_WarmingFootprint(Data_TS, Data_Date, Data_TimeRange)
+	Footprint, Footprint_pvalue = Create_WarmingFootprint(Data_TS)
 
 	# Output to nc file
 	Output_WarmingFootprint(Footprint, Footprint_pvalue)
